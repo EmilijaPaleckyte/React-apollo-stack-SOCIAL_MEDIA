3ae088a7bfc0a0c1e44afd2f01dae945f77e4329
EmilijaPaleckyte <epaleckyte@gmail.com>
2024-05-14 08:48:02 +0300

    auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import { AuthProvider, useAuth } from "./components/authcontext"; // Ensure useAuth is available
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 
 import Header from "./components/header";
 import Home from "./components/home";
@@ -10,26 +15,32 @@ import UserProfile from "./components/profile";
 
 function App() {
   return (
-      <AuthProvider>
-        <PostsProvider>
-          <Router>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/signup" element={<SignUpForm />} />
-              <Route path="/signin" element={<SignInForm />} />
-              <Route path="/profile" element={<UserProfileWrapper />} />
-            </Routes>
-          </Router>
-        </PostsProvider>
-      </AuthProvider>
+    <AuthProvider>
+      <PostsProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/signin" element={<SignInForm />} />
+            <Route path="/profile" element={<ProtectedUserProfile />} />
+          </Routes>
+        </Router>
+      </PostsProvider>
+    </AuthProvider>
   );
 }
 
-// Wrapper component to pass the username prop
-const UserProfileWrapper = () => {
+// Wrapper component to pass the username prop only if authenticated
+const ProtectedUserProfile = () => {
   const { user } = useAuth(); // Retrieve the user from the context
-  return <UserProfile username={user?.username} />; // Pass the username prop
+
+  // If user is authenticated, render the UserProfile component, otherwise redirect to signin page
+  return user ? (
+    <UserProfile username={user?.username} />
+  ) : (
+    <Navigate to="/signin" />
+  );
 };
 
 export default App;
